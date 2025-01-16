307eeccc1b6631b44130227fa6cded305716283b
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2025-01-16 11:50:49 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,38 @@ import { Edit, DeleteOutline } from "@mui/icons-material";
 
 function App() {
   const [TaskVal, setTaskVal] = useState("");
-  const [TaskList, setTaskList] = useState(
-    JSON.parse(localStorage.getItem("task-list") || "[]")
-  );
+  const [TaskList, setTaskList] = useState(JSON.parse(localStorage.getItem("task-list") || "[]"));
 
+  // Update localStorage whenever the TaskList state changes
   useEffect(() => {
     localStorage.setItem("task-list", JSON.stringify(TaskList));
   }, [TaskList]);
 
-  // Add task to task list
+  // add task to the task list
   const addTask = () => {
     if (TaskVal.trim() !== "") {
       setTaskList([
         ...TaskList,
         { id: Date.now(), title: TaskVal, checked: false },
       ]);
-      setTaskVal("");
+      setTaskVal(""); 
     }
   };
 
-  // Remove task
+  // Remove the task from the list
   const RemoveTask = (id) => {
     setTaskList(TaskList.filter((task) => task.id !== id));
   };
 
-  // Update task
+  // Update an existing task by filling the input with the task's title
   const UpdateTask = (task) => {
-    RemoveTask(task.id);
+    // Remove the task from the list
+    RemoveTask(task.id); 
+    // Fill the input by task title
     setTaskVal(task.title);
   };
 
-  // Toggle task checked state
+  // Toggle the task checked state
   const ToggleTaskChecked = (id) => {
     setTaskList(
       TaskList.map((task) =>
@@ -42,7 +43,7 @@ function App() {
     );
   };
 
-  // Render tasks
+  // Render the list of tasks
   const AllTasks = TaskList.map((task) => {
     return (
       <li key={task.id} className="flex items-center gap-1 py-2">
@@ -50,20 +51,26 @@ function App() {
           type="checkbox"
           id={task.id}
           checked={task.checked}
-          onChange={() => ToggleTaskChecked(task.id)}
+          onChange={() => ToggleTaskChecked(task.id)} // Toggle task checked state
         />
         <label
-          for={task.id}
+          htmlFor={task.id}
           className={`grow ${task.checked ? "line-through text-gray-500" : ""}`}
         >
-        {task.title}
+          {task.title}
         </label>
         <div className="flex items-center gap-1">
-          <button onClick={() => RemoveTask(task.id)} className="flex justify-center items-center bg-red-600 h-[2rem] aspect-square rounded-lg">
-            <DeleteOutline className="text-white" fontSize="small"/>
+          <button
+            onClick={() => RemoveTask(task.id)}
+            className="flex justify-center items-center bg-red-600 h-[2rem] aspect-square rounded-lg"
+          >
+            <DeleteOutline className="text-white" fontSize="small" />
           </button>
-          <button onClick={() => UpdateTask(task)} className="flex justify-center items-center bg-green-600 h-[2rem] aspect-square rounded-lg">
-            <Edit className="text-white" fontSize="small"/>
+          <button
+            onClick={() => UpdateTask(task)}
+            className="flex justify-center items-center bg-green-600 h-[2rem] aspect-square rounded-lg"
+          >
+            <Edit className="text-white" fontSize="small" />
           </button>
         </div>
       </li>
@@ -73,22 +80,20 @@ function App() {
   return (
     <div className="todo-container px-2 py-2 bg-white rounded-lg w-[300px] max-w-[90%]">
       <h1 className="text-2xl font-bold">To-Do List</h1>
-        <input
-          type="text"
-          placeholder="Your task here"
-          className="w-full py-2 px-3 border border-gray-400 rounded-md outline-none md:w-full"
-          value={TaskVal}
-          onChange={(e) => {
-            setTaskVal(e.target.value);
-          }}
-        />
-        <button
-          onClick={addTask}
-          disabled={!TaskVal.trim()}
-          className={`w-full mt-1 p-2 bg-cyan-500 rounded-md text-white  ${!TaskVal.trim() ? "opacity-50" : ""}`} 
-        >
-          Add Task
-        </button>
+      <input
+        type="text"
+        placeholder="Your task here"
+        className="w-full py-2 px-3 border border-gray-400 rounded-md outline-none md:w-full"
+        value={TaskVal}
+        onChange={(e) => setTaskVal(e.target.value)} // Update task input value
+      />
+      <button
+        onClick={addTask}
+        disabled={!TaskVal.trim()}
+        className={`w-full mt-1 p-2 bg-cyan-500 rounded-md text-white  ${!TaskVal.trim() ? "opacity-50" : ""}`}
+      >
+        Add Task
+      </button>
       <ul className="divide-y">{AllTasks}</ul>
     </div>
   );
